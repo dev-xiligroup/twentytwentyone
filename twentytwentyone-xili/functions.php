ed5d3a4a6e025889a3fdfa7a32c38c094a192509
dev-xiligroup <team@xiligroup.org>
2021-05-26 09:39:35 +0200

    Update functions.php

--- twentytwentyone-xili/functions.php
+++ twentytwentyone-xili/functions.php
@@ -1,8 +1,13 @@
 <?php
-// dev.xiligroup.com - msc - 2021-04-07 - first implementation (1.2)
+/**
+ * All to define child theme twentytwentyone-xili.
+ *
+ * @package dev.xiligroup.com - msc - 2021-04-07 - first implementation (1.2).
+ */
 
-define( 'TWENTYTWENTYONE_XILI_VER', '1.2' ); // as parent style.css
-
+/**
+ * Function twentytwentyone_xilidev_setup [Create child features].
+ */
 function twentytwentyone_xilidev_setup() {
 
 	$theme_domain = 'twentytwentyone';
@@ -30,7 +35,7 @@ function twentytwentyone_xilidev_setup() {
 		global $xili_language_theme_options; // used on both side
 		// Args dedicated to this theme named twenty twentyone .
 		$xili_args = array(
-			'customize_clone_widget_containers' => true, // comment or set to true to clone widget containers
+			'customize_clone_widget_containers' => true, // comment or set to true to clone widget containers !
 			'settings_name' => 'xili_2021_theme_options', // name of array saved in options table .
 			'theme_name' => 'Twenty Twentyone',
 			'theme_domain' => $theme_domain,
@@ -39,19 +44,18 @@ function twentytwentyone_xilidev_setup() {
 
 		add_action( 'widgets_init', 'twentytwentyone_xili_add_widgets' );
 
-		// new in WP 4.1 - now in XL 2.17.1
+		// new in WP 4.1 - now in XL 2.17.1 !
 		if ( ! has_filter( 'get_the_archive_description', array( $xili_language, 'get_the_archive_description' ) ) ) {
 			add_filter( 'get_the_archive_description', 'xili_get_the_archive_description' );
 		}
 
 		if ( is_admin() ) {
 
-			// Admin args dedicaced to this theme
-
+			// Admin args dedicaced to this theme !
 			$xili_admin_args = array_merge(
 				$xili_args,
 				array(
-					'customize_adds' => true, // add settings in customize page
+					'customize_adds' => true, // add settings in customize page !
 					'customize_addmenu' => false,
 					'capability' => 'edit_theme_options',
 					'authoring_options_admin' => false,
@@ -64,7 +68,7 @@ function twentytwentyone_xilidev_setup() {
 			} else {
 				$class_ok = false;
 			}
-		} else { // visitors side - frontend
+		} else { // visitors side - frontend !
 
 			if ( class_exists( 'Xili_Language_Theme_Options' ) ) {
 				$xili_language_theme_options = new Xili_Language_Theme_Options( $xili_args );
@@ -73,44 +77,44 @@ function twentytwentyone_xilidev_setup() {
 				$class_ok = false;
 			}
 		}
-		// new ways to add parameters in authoring propagation
+		// new ways to add parameters in authoring propagation !
 		add_theme_support(
 			'xiliml-authoring-rules',
 			array(
-				'post_format' => array(
-					'default' => '1',
-					'data' => 'attribute',
-					'hidden' => '1',
-					'name' => 'Post Format',
+				'post_format'  => array(
+					'default'     => '1',
+					'data'        => 'attribute',
+					'hidden'      => '1',
+					'name'        => 'Post Format',
 					/* translators: added in child functions by xili */
 					'description' => __( 'Will copy post_format in the future translated posts', 'twentytwentyone' ),
 				),
 				'post_content' => array(
-					'default' => '',
-					'data' => 'post',
-					'hidden' => '',
-					'name' => 'Post Content',
+					'default'     => '',
+					'data'        => 'post',
+					'hidden'      => '',
+					'name'        => 'Post Content',
 					/* translators: added in child functions by xili */
 					'description' => __( 'Will copy content in the future translated post', 'twentytwentyone' ),
 				),
-				'post_parent' => array(
-					'default' => '1', // (checked)
-					'data' => 'post',
-					'name' => 'Post Parent',
-					'hidden' => '', // checkbox not visible in dashboard UI
+				'post_parent'  => array(
+					'default'     => '1', // (checked).
+					'data'        => 'post',
+					'name'        => 'Post Parent',
+					'hidden'      => '', // checkbox not visible in dashboard UI !
 					/* translators: added in child functions by xili */
 					'description' => __( 'Will copy translated parent id (if original has parent and translated parent)!', 'twentytwentyone' ),
 				),
 			)
-		); //
+		);
 
 		if ( $class_ok ) {
 			$xili_theme_options = get_theme_xili_options();
-			// to collect checked value in xili-options of theme
+			// to collect checked value in xili-options of theme.
 			if ( file_exists( $xili_functionsfolder . '/multilingual-permalinks.php' ) && $xili_language->is_permalink && isset( $xili_theme_options['perma_ok'] ) && $xili_theme_options['perma_ok'] ) {
-				require_once( $xili_functionsfolder . '/multilingual-permalinks.php' ); // require subscribing premium services
+				require_once $xili_functionsfolder . '/multilingual-permalinks.php'; // require subscribing premium services.
 			}
-			if ( $xl_required_version ) { // msg choice is inside class
+			if ( $xl_required_version ) { // msg choice is inside class.
 				$msg = $xili_language_theme_options->child_installation_msg( $xl_required_version, $minimum_xl_version, $class_ok );
 			} else {
 				$msg = '
@@ -140,9 +144,9 @@ function twentytwentyone_xilidev_setup() {
 	}
 
 	// errors and installation informations
-	// after activation and in themes list
+	// after activation and in themes list.
 	if ( isset( $_GET['activated'] ) || ( ! isset( $_GET['activated'] ) && ( ! $xl_required_version || ! $class_ok ) ) ) {
-		// replace createfunction - obsolete in php 7.2
+		// replace createfunction - obsolete in php 7.2 !
 		add_action(
 			'admin_notices',
 			function() use ( &$msg ) {
@@ -152,9 +156,8 @@ function twentytwentyone_xilidev_setup() {
 	}
 
 	// end errors...
-	add_filter( 'pre_option_link_manager_enabled', '__return_true' ); // comment this line if you don't want links/bookmarks features
+	add_filter( 'pre_option_link_manager_enabled', '__return_true' ); // comment this line if you don't want links/bookmarks features.
 
-	//remove_filter( 'walker_nav_menu_start_el', 'twentynineteen_nav_description');
 	add_filter( 'twentytwentyone_default_hue', 'twentytwentyone_xili_default_hue' );
 	add_filter( 'twentytwentyone_default_saturation', 'twentytwentyone_xili_default_saturation' );
 	add_filter( 'twentytwentyone_default_lightness', 'twentytwentyone_xili_default_lightness' );
@@ -176,41 +179,51 @@ function my_theme_enqueue_styles() {
 		$theme->parent()->get( 'Version' )
 	);
 
-	wp_register_style( '2021-child-style', get_stylesheet_directory_uri() . '/style.css',
+	wp_register_style(
+		'2021-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
 		array( $parenthandle ),
 		$theme->get( 'Version' ) // this only works if you have Version in the style header.
 	);
 	wp_enqueue_style( '2021-child-style' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
+/**
+ * Function twentytwentyone_xili_custom_colors_css [to do].
+ *
+ * @param    <type> $theme_css     The theme css.
+ * @param    <type> $primary_color The primary color.
+ * @param    <type> $saturation    The saturation.
+ *
+ * @return   <type> The xili custom colors css.
+ */
 function twentytwentyone_xili_custom_colors_css( $theme_css, $primary_color, $saturation ) {
 	return $theme_css;
 }
 
-add_action( 'after_setup_theme', 'twentytwentyone_xilidev_setup', 11 ); // called after parent
+add_action( 'after_setup_theme', 'twentytwentyone_xilidev_setup', 11 ); // called after parent.
 
-// Enable shortcodes in text widgets
+// Enable shortcodes in text widgets.
 add_filter( 'widget_text', 'do_shortcode' );
 
 /**
- * define when search form is completed by radio buttons to sub-select language when searching
- *
+ * Define when search form is completed by radio buttons to sub-select language when searching.
  */
 function special_head() {
 	if ( class_exists( 'xili_language' ) ) { // if temporary disabled
 		// to change search form of widget
 		// if ( is_front_page() || is_category() || is_search() )
 		if ( is_front_page() || is_search() || is_404() ) {
-			add_filter( 'get_search_form', 'my_langs_in_search_form_2021', 10, 1 ); // here below
+			add_filter( 'get_search_form', 'my_langs_in_search_form_2021', 10, 1 ); // here below.
 		}
 	}
 }
 add_action( 'wp_head', 'special_head', 11 );
 
 /**
- * add search other languages in form - see functions.php when fired
+ * Add search other languages in form - see functions.php when fired
  *
+ * @param    html $the_form search form.
  */
 function my_langs_in_search_form_2021( $the_form ) {
 	global $xili_language;
@@ -226,23 +239,31 @@ function my_langs_in_search_form_2021( $the_form ) {
 }
 
 
-if ( class_exists( 'xili_language' ) ) { // if temporary disabled
+if ( class_exists( 'xili_language' ) ) { // if temporary disabled !
 	add_action( 'after_setup_theme', 'theme_mod_create_array', 11, 1 );
-
+	/**
+	 * Function theme_mod_create_array [to do].
+	 */
 	function theme_mod_create_array() {
 		global $xili_language;
 		if ( method_exists( $xili_language, 'set_theme_mod_to_be_filtered' ) ) {
-			$xili_language->set_theme_mod_to_be_filtered( 'copyright' ); // used in footer
+			$xili_language->set_theme_mod_to_be_filtered( 'copyright' ); // used in footer !
 		}
 	}
 }
 
+/**
+ * Function twentytwentyone_xili_add_widgets [to do].
+ */
 function twentytwentyone_xili_add_widgets() {
 	if ( class_exists( 'Xili_Widget_Categories' ) ) {
 		register_widget( 'Xili_Widget_Categories' ); // in xili-language-widgets.php since 2.16.3
 	}
 }
 
+/**
+ * Function twentytwentyone_xili_header_image [to do].
+ */
 function twentytwentyone_xili_header_image() {
 
 	$header_image_url = get_header_image();
@@ -250,7 +271,6 @@ function twentytwentyone_xili_header_image() {
 	$text_color = get_header_textcolor();
 
 	// If we get this far, we have custom styles.
-
 	if ( $header_image_url ) {
 
 			/**
@@ -301,7 +321,7 @@ function twentytwentyone_xili_header_image() {
 				}
 			}
 		}
-	?>
+		?>
 
 	<div class="header-image">
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
@@ -309,10 +329,13 @@ function twentytwentyone_xili_header_image() {
 		</a>
 	</div>
 
-	<?php
+		<?php
 	}
 }
 
+/**
+ * Function twentytwentyone_xilidev_setup_custom_header [to do].
+ */
 function twentytwentyone_xilidev_setup_custom_header() {
 
 	// %2$s = in child.
@@ -346,6 +369,7 @@ function twentytwentyone_xilidev_setup_custom_header() {
 	add_theme_support( 'custom-header', $args ); // need 8 in add_action to overhide parent .
 
 }
+
 /**
  * [twentytwentyone_xili_credits description]
  */
@@ -373,7 +397,13 @@ function twentytwentyone_xili_credits() {
 add_action( 'twentytwentyone_xili_credits', 'twentytwentyone_xili_credits' );
 
 
-// testing .
+/**
+ * Function twentytwentyone_xili_default_hue [to do].
+ *
+ * @param    <type> $value The value.
+ *
+ * @return   <type> The xili default hue.
+ */
 function twentytwentyone_xili_default_hue( $value ) {
 	return $value; // 184;
 }
